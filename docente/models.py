7ae807dfabee9b46039f33d7523b4b35c9bab9cc
Julio Hurtado <juliens@outlook.com>
2016-07-16 23:19:54 -0500

    Agregar grupos de usuario a los modelos Control de permisos

--- docente/models.py
+++ docente/models.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 from django.db import models
+
 
 # Create your models here.
 
 class Docente(User):
-    cedula = models.CharField(unique=True, max_length=16)
+	cedula = models.CharField(unique=True, max_length=16)
 
-    def __unicode__(self):
-        return self.first_name + " " + self.last_name
+	def __unicode__(self):
+		return self.first_name + " " + self.last_name
+
+	def clean(self):
+		if (Group.objects.filter(name='Docentes').exists()):
+			grupo = Group.objects.get(name='Docentes')
+			self.user.groups.add(grupo)
+		else:
+			grupo = Group.objects.create(name="Docentes")
+			self.user.groups.add(grupo)
+		return self
--- estudiante/models.py
+++ estudiante/models.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 from django.db import models
 
 # Create your models here.
 class Estudiante(User):
     cedula = models.CharField(unique=True, max_length=16)
+
+    def clean(self):
+        if (Group.objects.filter(name='Estudiantes').exists()):
+            grupo = Group.objects.get(name='Estudiantes')
+            self.user.groups.add(grupo)
+        else:
+            grupo = Group.objects.create(name="Estudiantes")
+            self.user.groups.add(grupo)
+        return self
