b23df110f92e85bdd55e396dc699c16929cbce9e
Julio Hurtado <juliens@outlook.com>
2016-07-30 12:59:13 -0500

    Heroku app

--- samms/settings.py
+++ samms/settings.py
@@ -166,5 +166,5 @@ except ImportError:
 STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR,'static/'),
+    os.path.join(BASE_DIR,'static/'),
 )
